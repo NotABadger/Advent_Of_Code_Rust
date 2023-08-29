27c5868e4fb75203e76fafb1051959fc124981fb
Matthew <mathieu-fennema@live.nl>
2023-08-29 20:58:54 +0200

    Finished day 12

--- 2015/day_12/src/main.rs
+++ 2015/day_12/src/main.rs
@@ -1,7 +1,4 @@
-// mod file_processor_file;
-// mod json;
 mod file_processor_file;
-
 use file_processor_file::FileProcessor;
 
 
@@ -24,38 +21,51 @@ fn main() {
     }
 
     let mut tot_val: i32 = 0;
-    get_sum_of_JSON_contents(&parsed, &mut tot_val);
+    get_sum_of_JSON_value(&parsed, &mut tot_val);
     println!("Total value is: {}", tot_val);
 
 }
 
 #[allow(non_snake_case)]
-fn get_sum_of_JSON_contents(json_object: &json::JsonValue, sum_total: &mut i32)
+fn get_sum_of_JSON_value(json_object: &json::JsonValue, sum_total: &mut i32)
 {
-
-    for object in json_object.entries()
+    if json_object.is_array()
     {
-        if object.1.is_object()
+        for json_arr_itt in json_object.members()
         {
-            get_sum_of_JSON_contents(object.1,  sum_total);
-        }
-        else if object.1.is_number()
-        {
-            *sum_total += object.1.as_i32().expect("Casting Json nr to i32 failed");
-        }
-        else if object.1.is_array()
-        {
-            for json_arr_itt in object.1.members()
+            if json_arr_itt.is_number()
             {
-                if json_arr_itt.is_number()
-                {
-                    *sum_total += json_arr_itt.as_i32().expect("Casting Json nr to i32 failed");
-                }
-                if object.1.is_object()
-                {
-                    get_sum_of_JSON_contents(object.1,  sum_total);
-                }
+                *sum_total += json_arr_itt.as_i32().expect("Casting Json nr to i32 failed");
             }
+            if json_arr_itt.is_object() || json_arr_itt.is_array()
+            {
+                get_sum_of_JSON_value(json_arr_itt,  sum_total);
+            }
+        }
+    }
+    else if json_object.is_object()
+    {
+        let mut object_painted_red: bool = false;
+        let mut object_value: i32 = 0;
+        for object in json_object.entries()
+        {
+            if object.1.is_string() && object.1.eq("red")
+            {
+                object_painted_red = true;
+                break;
+            }
+            if object.1.is_object() || object.1.is_array()
+            {
+                get_sum_of_JSON_value(object.1,  &mut object_value);
+            }
+            else if object.1.is_number()
+            {
+                object_value += object.1.as_i32().expect("Casting Json nr to i32 failed");
+            }
+        }
+        if !object_painted_red //ignore this if-statement, and always add object value for part 1
+        {
+            *sum_total += object_value;
         }
     }
 }
