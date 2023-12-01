073deec0c6424db947682ecdc7d086edb9cfc5ce
Mathieu Fennema <Mathieu.Fennema@vanderlande.com>
2023-12-01 11:09:15 +0100

    Updated error handling

--- 2023/input_file_lib/src/lib.rs
+++ 2023/input_file_lib/src/lib.rs
@@ -1,7 +1,8 @@
 mod file_reader;
 mod parse_commandline_args;
 
-pub fn read_file_content() -> Option<String>
+
+pub fn read_file_content() -> Result<String,  Box<dyn std::error::Error>>
 {
     let arguments: Vec<String> = parse_commandline_args::retrieve_commandline_args();
     match arguments.get(1)
@@ -9,17 +10,12 @@ pub fn read_file_content() -> Option<String>
         Some(first_parameter) => {
             if file_reader::check_file_exists(first_parameter)
             {
-                return Some(file_reader::read_file(first_parameter).expect("Tried to read a file that should exist, but does not."));
+                return Ok(file_reader::read_file(first_parameter)?);
             }
-            else {
-                println!("I did find a parameter, but its not a file");
-                return None;
-            }
+            return Err("File does not exist".into());
         }
-        
         None => {
-            println!("could not find any parameter, or parameter file does not exist");
-            None
+            Err("could not find any parameter, or parameter file does not exist".into())
         }
     }
 }
