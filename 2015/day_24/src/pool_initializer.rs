29efc23e896188e1f050a842ae29aa8e09df461f
Mathieu Fennema <Mathieu.fennema@vanderlande.com>
2024-03-28 10:12:45 +0100

    Added a threadpool lib

--- 2015/day_24/src/pool_initializer.rs
+++ 2015/day_24/src/pool_initializer.rs
@@ -2,6 +2,7 @@ pub extern crate threadpool;
 pub use threadpool::ThreadPool;
 extern crate num_cpus;
 
+//https://docs.rs/threadpool/latest/threadpool/
 pub fn initialize_threadpool() -> ThreadPool {
     ThreadPool::new(num_cpus::get())
 }
